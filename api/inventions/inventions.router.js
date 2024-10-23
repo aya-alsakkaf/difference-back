c0270808d2c621cbe430263c83e3e4062505da36
Yusuf Alnasiri <alnasiriyusuf@gmail.com>
2024-10-23 18:06:21 +0300

    fixes

--- api/inventions/inventions.router.js
+++ api/inventions/inventions.router.js
@@ -6,7 +6,11 @@ const {
   deleteInvention,
 } = require("./inventions.controllers");
 const upload = require("../../middleware/multer");
-
+const passport = require("passport");
 inventionRouter.get("/", getInventions);
 inventionRouter.post("/", upload.array("images", 30), createInvention);
-inventionRouter.put("/:id", updateInvention);
+inventionRouter.put("/:id", passport.authenticate("jwt", { session: false }), updateInvention);
+inventionRouter.delete("/:id", passport.authenticate("jwt", { session: false }), deleteInvention);
+
+module.exports = inventionRouter;
+
