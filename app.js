09a9362dd2e3fb47451129f9c39597ffd2286764
AbdulazizMurad <abdulaziz.murad2001@gmail.com>
2024-10-23 21:18:33 +0300

    aaa

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const path = require("path")
+const express = require("express");
+const path = require("path");
 const app = express();
-const connectDb = require('./database');
-const passport = require('passport');
-const morgan = require('morgan');
-const cors = require("cors")
-const notFoundHandler = require('./middleware/notFoundHandler');
-const errorHandler = require('./middleware/errorHandler');
-const userRouter = require("./api/users/user.router")
-const inventionRouter = require("./api/inventions/inventions.router")
-const orderRouter = const categoryRouter = require("./api/categories/categories.router");
-require("./api/orders/orders.routes")
-require("dotenv").config()
+const connectDb = require("./database");
+const passport = require("passport");
+const morgan = require("morgan");
+const cors = require("cors");
+const notFoundHandler = require("./middleware/notFoundHandler");
+const errorHandler = require("./middleware/errorHandler");
+const userRouter = require("./api/users/user.router");
+const inventionRouter = require("./api/inventions/inventions.router");
+const orderRouter = require("./api/orders/orders.routes");
+const categoryRouter = require("./api/categories/categories.router");
+
+require("dotenv").config();
 connectDb();
 app.use(express.json());
-app.use(cors())
-app.use(morgan("dev"))
+app.use(cors());
+app.use(morgan("dev"));
 
 app.use(passport.initialize());
-require('./middleware/passport'); 
+require("./middleware/passport");
 app.use("/api", userRouter);
-app.use("/api", inventionRouter);
-app.use("/api", categoryRouter);
-app.use("/media", express.static(path.join(__dirname, "/media")))
-app.use(notFoundHandler)
-app.use(errorHandler)
+app.use("/api/orders", orderRouter);
+app.use("/api/inventions", inventionRouter);
+app.use("/api/categories", categoryRouter);
+app.use("/media", express.static(path.join(__dirname, "/media")));
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`The application is running on localhost:${process.env.PORT}`);
